e0f53818916d989fe670c6516a1fb29214c55e77
Bousquet <bousquet.laurent12@gmail.com>
2022-01-02 21:57:43 +0100

    feat: decode ok

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{arg, Arg, App, AppSettings};
+use std::str;
 
 extern crate base64;
 
@@ -6,6 +7,18 @@ fn to_base64(to_convert: &str) {
     println!("Converting {}", to_convert);
     let b64 = base64::encode(to_convert);
     println!("{}", b64);
+}
+
+fn from_base64(to_convert: &str) {
+    println!("Converting {}", to_convert);
+    let decoded = base64::decode(to_convert).unwrap();
+
+    let s = match str::from_utf8(&decoded) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+
+    println!("{}", s);
 }
 
 fn main() {
@@ -21,8 +34,15 @@ fn main() {
                             .short('e')
                             .long("encode")
                             .help("Encode the provided string to base64")
+                            .conflicts_with("decode")
                 )
-                .arg(arg!(<STRING> "The string to encode or decode to base64"))
+                .arg(Arg::new("decode")
+                            .short('d')
+                            .long("decode")
+                            .help("Decode the provided base64")
+                            .conflicts_with("encode")
+                )
+                .arg(arg!(<STRING> "The string to encode or decode"))
                 .setting(AppSettings::ArgRequiredElseHelp),
         )
         .get_matches();
@@ -30,7 +50,11 @@ fn main() {
     match matches.subcommand() {
         Some(("base64", sub_matches)) => {
             let value = sub_matches.value_of("STRING").expect("required");
-            to_base64(value);
+            if sub_matches.is_present("decode") {
+                from_base64(value);
+            } else {
+                to_base64(value);
+            }
         }
         Some((ext, sub_matches)) => {
             let args = sub_matches
